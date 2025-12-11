250b291ac0c1956bbe4220b72028459fc066976c
Arseniy Naumov <ivanov262ivanov@gmail.com>
2025-12-11 12:50:33 +0200

    add task solution

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,13 @@
 import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
+export const Sum = ({a = 0, b = 0, }) => {
+    let sum = a + b;
+    return(
+    <p>Sum of {a} and {b} is {sum}</p>)
 
+};
+export default Sum;
 createRoot(document.getElementById('root')).render(<App />);
+
+
