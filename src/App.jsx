d85ea9c5915c847c7f775ddb0f3271a4c2ae2c28
Arseniy Naumov <ivanov262ivanov@gmail.com>
2025-12-11 13:34:08 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.scss';
-import Sum from './index';
+import {Sum} from './index';
 export const App = () => (
   <>
-  <Sum a={2} b={3} />
-  <Sum a={-5} b={5} />
-  <Sum a={10} b={0} />
-  <Sum a={0} b={5} />
-  <Sum a={0} b={0} />
+  <Sum />
+  <Sum />
+  <Sum/>
+  <Sum />
+  <Sum />
     {/* Replace paragraphs with Sum componets */}
     {/* And remove commented lines :) */}
   </>
--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,8 @@ import { App } from './App';
 export const Sum = ({a = 0, b = 0, }) => {
     let sum = a + b;
     return(
-    <p>Sum of {a} and {b} is {sum}</p>)
-
+    <p>{`Sum of ${a} and ${b} is ${sum}`}</p>)
 };
-export default Sum;
 createRoot(document.getElementById('root')).render(<App />);
 
 
